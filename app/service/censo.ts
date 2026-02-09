042adb1886d76bdc992e924c26c3b9f6bc00470b
Antonio Costa dos Santos <costaantonio883@gmail.com>
2026-02-09 18:00:01 -0300

    feat(ui) ajuste de permissao

--- app/service/censo.ts
+++ app/service/censo.ts
@@ -1,19 +1,33 @@
 import { runQueryCenso } from "../database/mysql";
 import { formatTelefone } from "../helpers/formatters";
 
+interface UserCenso {
+  nome: string;
+  status: string;
+  cidade: string;
+  datanasc: string;
+  email: string;
+  cel: string;
+  adm: string;
+  id_grupo: number;
+  grupo: string;
+  cor: string;
+}
+
 export const censoService = {
-  getUserByNumber: async (number: string) => {
+  getUserByNumber: async (number: string): Promise<UserCenso | null> => {
     const telefoneFormatado = formatTelefone(number);
     const sql = `
-                SELECT nome, status, func_mes
-                FROM usuarios
-                WHERE func_mes = 'S'
-                AND status = 'A'
-                AND cel = ?
+                SELECT u.nome, u.status, u.cidade, u.datanasc, u.email, u.cel, u.adm, p.id as id_grupo, p.grupo, p.cor 
+                FROM usuarios as u
+                LEFT JOIN cs_gp_usuarios as p ON p.id = u.grupo_id
+                WHERE u.status = 'A' AND u.cel = ?;
             `;
-    const result = (await runQueryCenso(sql, [telefoneFormatado])) as any;
+    const result = (await runQueryCenso(sql, [
+      telefoneFormatado,
+    ])) as UserCenso[];
     if (result.length > 0) {
-      return result;
+      return result[0];
     } else {
       return null;
     }
