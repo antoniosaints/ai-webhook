f813dcec2fb190cc66d981bbd854a113009d337b
Antonio Costa dos Santos <costaantonio883@gmail.com>
2026-02-11 08:12:26 -0300

    feat(ui) ajuste de UI

--- app/service/ixc.ts
+++ app/service/ixc.ts
@@ -2,6 +2,7 @@ import { runQueryIXC } from "../database/mysql";
 
 interface ClienteIXC {
   nome: string;
+  cidade: string;
   data_ativacao: string;
   data_renovacao: string;
   data_validade: string;
@@ -12,9 +13,10 @@ interface ClienteIXC {
 export const ixcService = {
   getNomeClienteByContrato: async (contrato: number) => {
     const sql = `
-                SELECT c.razao as nome, ct.data_ativacao, ct.data_renovacao, ct.data_validade, ct.status, ct.status_internet
+                SELECT c.razao as nome, ci.nome as cidade, ct.data_ativacao, ct.data_renovacao, ct.data_validade, ct.status, ct.status_internet
                 FROM ixcprovedor.cliente_contrato as ct 
                 LEFT JOIN ixcprovedor.cliente as c ON c.id = ct.id_cliente
+                LEFT JOIN ixcprovedor.cidade as ci ON c.cidade = ci.id
                 WHERE ct.id = ?;
                     `;
     const result = (await runQueryIXC(sql, [contrato])) as ClienteIXC[];
